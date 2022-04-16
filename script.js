9979543c7a7391459fffe9191781e8ed812129f6
Mikael <mk.mikael159@gmail.com>
2022-04-16 18:03:54 -0300

    Altera algumas funcionalidades referente aos campos de texto

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 var input = document.querySelector('textarea#campo');
 var input2 = document.querySelector('textarea#campo2');
+input.focus();
 
 // FUNÇÃO PARA CONVERTER A MENSAGEM (CODIFICAR)
 function codificar() {
-    // LIMPA O CAMPO TODA A VEZ QUE A FUNÇÃO É CHAMADA
-    document.getElementById('none').innerHTML = '';
-    document.querySelector('img.icone').style.display = 'none';
+    if (input.value.length == 0) {
+        alert('Digite algum texto no campo abaixo');
+        input.focus();
+    } else {
+        // LIMPA O CAMPO TODA A VEZ QUE A FUNÇÃO É CHAMADA
+        document.getElementById('none').innerHTML = '';
+        document.querySelector('img.icone').style.display = 'none';
 
-    var texto = input.value
-    var txt =  texto.replace(/e/igm, 'enter');
-    var txt = txt.replace(/i/igm, 'imes');
-    var txt = txt.replace(/a/igm, 'ai');
-    var txt = txt.replace(/o/igm, 'ober');
-    var txt = txt.replace(/u/igm, 'ufat');
+        var texto = input.value
+        var txt =  texto.replace(/e/igm, 'enter');
+        var txt = txt.replace(/i/igm, 'imes');
+        var txt = txt.replace(/a/igm, 'ai');
+        var txt = txt.replace(/o/igm, 'ober');
+        var txt = txt.replace(/u/igm, 'ufat');
 
-   document.getElementById('campo2').innerHTML = `${txt}`;
+        document.getElementById('campo2').innerHTML = `${txt}`;
 
-   // CRIA O BOTÃO DE COPIAR TEXTO QUANDO A FUNÇÃO "CODIFICAR()" É CHAMADA
-   document.getElementById('copy').innerHTML = '<button class="botao botao3" onclick="copy()">Copiar</button>';
+        // CRIA O BOTÃO DE COPIAR TEXTO QUANDO A FUNÇÃO "CODIFICAR()" É CHAMADA
+        document.getElementById('copy').innerHTML = '<button class="botao botao3" onclick="copy()">Copiar</button>';
+    }
 }
 // FUNÇÃO PARA DECODIFICAR O TEXTO
 function decodificar() {
-    // LIMPA O CAMPO TODA A VEZ QUE O BOTÃO É CLICADO
-    document.getElementById('none').innerHTML = '';
-    document.querySelector('img.icone').style.display = 'none';
-    document.getElementById('campo').innerHTML = '';
+    if (input.value.length == 0) {
+        alert('Digite algum texto no campo abaixo');
+        input.focus();
+    } else {
+        // LIMPA O CAMPO TODA A VEZ QUE O BOTÃO É CLICADO
+        document.getElementById('none').innerHTML = '';
+        document.querySelector('img.icone').style.display = 'none';
+        document.getElementById('campo').innerHTML = '';
 
-    var texto = input.value
-    var txt =  texto.replace(/enter/igm, 'e');
-    var txt = txt.replace(/imes/igm, 'i');
-    var txt = txt.replace(/ai/igm, 'a');
-    var txt = txt.replace(/ober/igm, 'o');
-    var txt = txt.replace(/ufat/igm, 'u');
- 
-    document.getElementById('campo2').innerHTML = `${txt}`;
+        var texto = input.value
+        var txt =  texto.replace(/enter/igm, 'e');
+        txt = txt.replace(/imes/igm, 'i');
+        txt = txt.replace(/ai/igm, 'a');
+        txt = txt.replace(/ober/igm, 'o');
+        txt = txt.replace(/ufat/igm, 'u');
+    
+        document.getElementById('campo2').innerHTML = `${txt}`;
+        
+        // CRIA O BOTÃO DE COPIAR TEXTO QUANDO A FUNÇÃO "CODIFICAR()" É CHAMADA
+        document.getElementById('copy').innerHTML = '<button class="botao botao3" onclick="copy()">Copiar</button>';
+    }
 }
 // FUNÇÃO PARA COPIAR O TEXTO CONVERTIDO
 function copy() {
@@ -44,9 +58,11 @@ function copy() {
 }
 
 
+
 var botao1 = document.querySelector('button.botao1');
 botao1.onclick = codificar;
 
 var botao2 = document.querySelector('button.botao2');
 botao2.onclick = decodificar;
+
 
